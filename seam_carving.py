4736d029d5b849463d78102e13f74265090573d1
mitchfriedman <Mitch@Mitchells-MBP-4.attlocal.net>
2015-06-18 19:32:00 -0700

    Clean up code and fix PEP issues

--- seam_carving.py
+++ seam_carving.py
@@ -1,31 +1,31 @@
 from PIL import Image as PILImage
 
 
+class Pixel(object):
+
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.color = color
+
+
 class Image(object):
 
     def __init__(self, size):
         self.width = size[0] - 1
         self.height = size[1] - 1
-
         self.color_matrix = [[0 for _ in range(self.width+1)] for _ in range(self.height+1)]
 
     def set_from_pil_image(self, pil_image):
-        print self.width, self.height
         for i in range(self.width+1):
             for j in range(self.height+1):
-                self.set_pixel((i, j), pil_image[i, j])
+                self.set_pixel((i, j), Pixel(i, j, pil_image[i, j]))
 
-    def set_pixel(self, position, color_tuple):
-        self.color_matrix[position[1]][position[0]] = color_tuple
+    def set_pixel(self, position, pixel):
+        self.color_matrix[position[1]][position[0]] = pixel
 
     def get_pixel(self, position):
-        return self.color_matrix[position[1]][position[0]]
-
-class Carver(object):
-    
-    def __init__(self, img_path):
-        self.image = PILImage.open(img_path)
-        self.matrix = self.image.load()
+        return self.color_matrix[position[1]][position[0]].color
 
     def x_grad(self, image, x, y):
         right = x + 1
@@ -59,7 +59,7 @@ class Carver(object):
         ry = r2-r1
         gy = g2-g1
         by = b2-b1
-        return ry ** 2 + gy **2 + by**2
+        return ry ** 2 + gy ** 2 + by ** 2
 
     def get_energy_of_pixel(self, image, x, y):
 
@@ -71,10 +71,18 @@ class Carver(object):
 
         return x_gradient_squared + y_gradient_squared
 
-    def build_graph(self, image):
-        return [[self.get_energy_of_pixel(image, x, y) for x in range(image.width+1)] for y in range(image.height+1)]
 
-    def get_shortest_path(self, graph):
+class Carver(object):
+
+    def __init__(self, img_path):
+        self.original_image = PILImage.open(img_path)
+        self.image = Image(self.original_image.size)
+        self.image.set_from_pil_image(self.original_image.load())
+
+    def build_graph(self, image):
+        return [[image.get_energy_of_pixel(image, x, y) for x in range(image.width+1)] for y in range(image.height+1)]
+
+    def get_shortest_vertical_path(self, graph):
         shortest = -1
         position = -1
         for i in range(len(graph[0])):
@@ -89,43 +97,38 @@ class Carver(object):
         return position
 
     def remove_shortest_seams(self, image, seams_to_remove):
-
         graph = self.build_graph(image)
 
         for i in range(seams_to_remove):
-            col_to_remove = self.get_shortest_path(graph)
+            col_to_remove = self.get_shortest_vertical_path(graph)
             graph = self._remove_column_from_graph(image.color_matrix, graph, col_to_remove)
-            image.width -= 1
+
+        image.width -= seams_to_remove
         return graph
 
-    def _remove_column_from_graph(self, matrix, graph, column):
-        for i in range(len(graph)):
-            graph[i].pop(column)
-            matrix[i].pop(column)
+    def shrink_image(self, by=None):
+        by = by or 0
+        self.remove_shortest_seams(self.image, by)
 
-        return graph
+        self.write_new_image('new_image.png', self.image)
 
-    def resize_image(self, image, width=100):
-        self.remove_shortest_seams(image, width)
-
+    def write_new_image(self, image_name, image):
         im = PILImage.new("RGB", (image.width, image.height))
         pixels = im.load()
-        print image.width
         for i in range(image.width):
             for j in range(image.height):
                 pixels[i, j] = image.get_pixel((i, j))
 
-        im.save("test123.png", "png")
-        print im
+        im.save(image_name, "png")
+
+    def _remove_column_from_graph(self, color_matrix, graph, column):
+        for i in range(len(graph)):
+            graph[i].pop(column)
+            color_matrix[i].pop(column)
+
+        return graph
 
 
 if __name__ == '__main__':
     carver = Carver("test.jpeg")
-    original_image = Image((carver.image.size[0], carver.image.size[1]))
-
-    original_image.set_from_pil_image(carver.image.load())
-
-    carver.resize_image(original_image, width=100)
-
-
-
+    carver.shrink_image(by=100)
--- test_seam_carver.py
+++ test_seam_carver.py
@@ -1,163 +1,151 @@
 import unittest
 from copy import deepcopy
-from seam_carving import Carver, Image
+from seam_carving import Carver, Image, Pixel
+
+test_matrix = [
+    [(255, 101, 51), (255, 101, 153), (255, 101, 255)],
+    [(255, 153, 51), (255, 153, 153), (255, 153, 255)],
+    [(255, 203, 51), (255, 204, 153), (255, 205, 255)],
+    [(255, 255, 51), (255, 255, 153), (255, 255, 255)]
+]
+
+graph = [
+    [
+        ((153 - 255)**2)**2 + ((255 - 153)**2)**2,  # row 1
+        ((255 - 51)**2)**2 + ((255 - 153)**2)**2,   # row 1
+        ((153 - 51)**2)**2 + ((255 - 153)**2)**2],  # row 1
+    [
+        ((255 - 153)**2)**2 + ((203 - 101)**2)**2,  # row 2
+        ((255 - 51)**2)**2 + ((204 - 101)**2)**2,   # row 2
+        ((153 - 51)**2)**2 + ((205 - 101)**2)**2],  # row 2
+    [
+        ((205 - 204)**2 + (255 - 153)**2)**2 + ((255 - 153)**2)**2,  # row 3
+        ((205 - 203)**2 + (255 - 51)**2)**2 + ((255 - 153)**2)**2,  # row 3
+        ((204 - 203)**2 + (153 - 51)**2)**2 + ((255 - 153)**2)**2   # row 3
+    ],
+    [
+        ((255 - 153)**2)**2 + ((203 - 101)**2)**2,  # row 4
+        ((255 - 51)**2)**2 + ((204 - 101)**2)**2,  # row 4
+        ((153 - 51)**2)**2 + ((205 - 101)**2)**2   # row 4
+    ]
+]
+
+
+def get_shortest_seam_index():
+    sums = []
+    for i in range(3):
+        sums.append(reduce(lambda x, y: x + y, graph[i]))
+    pos = sums.index(min(sums))
+    return pos
+
+
+def create_test_image():
+    image = Image((3, 4))
+
+    for i in range(4):
+        for j in range(3):
+            image.set_pixel((j, i), Pixel(i, j, test_matrix[i][j]))
+
+    return image
 
 
 carver = Carver("test.jpeg")
 
-class TestCarver(unittest.TestCase):
 
-    test_matrix = [
-        [(255,101,51), (255,101,153), (255,101,255)],
-        [(255,153,51), (255,153,153), (255,153,255)],
-        [(255,203,51), (255,204,153), (255,205,255)],
-        [(255,255,51), (255,255,153), (255,255,255)]
-    ]
-
-    graph = [
-        [
-            ((153 - 255)**2)**2 + ((255 - 153)**2)**2,  # row 1
-            ((255 -  51)**2)**2 + ((255 - 153)**2)**2,  # row 1
-            ((153 -  51)**2)**2 + ((255 - 153)**2)**2], # row 1
-        [
-            ((255 - 153)**2)**2 + ((203 - 101)**2)**2, # row 2
-            ((255 -  51)**2)**2 + ((204 - 101)**2)**2, # row 2
-            ((153 -  51)**2)**2 + ((205 - 101)**2)**2],# row 2
-        [
-            ((205 - 204)**2 + (255 - 153)**2)**2 + ((255 - 153)**2)**2, # row 3
-            ((205 - 203)**2 + (255 -  51)**2)**2 + ((255 - 153)**2)**2, # row 3
-            ((204 - 203)**2 + (153 -  51)**2)**2 + ((255 - 153)**2)**2  # row 3
-        ],
-        [
-            ((255 - 153)**2)**2 + ((203 - 101)**2)**2, # row 4
-            ((255 -  51)**2)**2 + ((204 - 101)**2)**2, # row 4
-            ((153 -  51)**2)**2 + ((205 - 101)**2)**2  # row 4
-        ]
-    ]
-
-    def _get_shortest_seam_index(self):
-        sums = []
-        for i in range(3):
-            sums.append(reduce(lambda x, y: x + y, self.graph[i]))
-        pos = sums.index(min(sums))
-        return pos
-
-
-    def _create_test_image(self):
-        image = Image((3, 4))
-
-        for i in range(4):
-            for j in range(3):
-                image.set_pixel((j, i), self.test_matrix[i][j])
-
-        return image
-
-    def test_create_image(self):
-        image = self._create_test_image()
-        self.assertEqual(self.test_matrix, image.color_matrix)
-
+class TestImage(unittest.TestCase):
     def test_x_grad_middle(self):
-        image = self._create_test_image()
+        image = create_test_image()
 
-        x_grad_1_2 = carver.x_grad(image, 1, 2)
-
-        # row 3 in test matrix
+        x_grad_1_2 = image.x_grad(image, 1, 2)  # row 3 in test matrix
         expected = (205 - 203)**2 + (255 - 51) ** 2
-
         self.assertEqual(x_grad_1_2, expected)
 
     def test_x_grad_top_left_corner(self):
-        image = self._create_test_image()
-
-        x_grad_0_0 = carver.x_grad(image, 0, 0)
-
+        image = create_test_image()
+        x_grad_0_0 = image.x_grad(image, 0, 0)
         expected = (153 - 255) ** 2
 
         self.assertEqual(expected, x_grad_0_0)
 
     def test_x_grad_bottom_right_corner(self):
-        image = self._create_test_image()
-
-        x_grad_2_3 = carver.x_grad(image, 2, 3)
-
+        image = create_test_image()
+        x_grad_2_3 = image.x_grad(image, 2, 3)
         expected = (153 - 51) ** 2
 
         self.assertEqual(expected, x_grad_2_3)
 
     def test_y_grad_middle(self):
-        image = self._create_test_image()
+        image = create_test_image()
 
-        y_grad_1_2 = carver.y_grad(image, 1, 2)
+        y_grad_1_2 = image.y_grad(image, 1, 2)
 
         expected = (255 - 153) ** 2
 
         self.assertEqual(expected, y_grad_1_2)
 
     def test_y_grad_top_left(self):
-        image = self._create_test_image()
+        image = create_test_image()
 
-        y_grad_0_0 = carver.y_grad(image, 0, 0)
+        y_grad_0_0 = image.y_grad(image, 0, 0)
         expected = (255 - 153) ** 2
 
         self.assertEqual(expected, y_grad_0_0)
 
     def test_y_grad_bottom_right(self):
-        image = self._create_test_image()
+        image = create_test_image()
 
-        y_grad_2_3 = carver.y_grad(image, 2, 3)
+        y_grad_2_3 = image.y_grad(image, 2, 3)
         expected = (205 - 101) ** 2
         self.assertEqual(expected, y_grad_2_3)
 
     def test_get_energy(self):
-        image = self._create_test_image()
+        image = create_test_image()
 
         expected = ((153 - 255) ** 2)**2 + ((255 - 153) ** 2)**2
 
-        self.assertEqual(expected, carver.get_energy_of_pixel(image, 0, 0))
+        self.assertEqual(expected, image.get_energy_of_pixel(image, 0, 0))
+
+
+class TestCarver(unittest.TestCase):
+
+    def test_create_image(self):
+        image = create_test_image()
+        for i in range(len(test_matrix[0])):
+            for j in range(len(test_matrix)):
+                self.assertEqual(test_matrix[j][i], image.get_pixel((i, j)))
 
     def test_build_graph(self):
-        image = self._create_test_image()
+        image = create_test_image()
+        self.assertEqual(graph, carver.build_graph(image))
 
-        test_against = carver.build_graph(image)
-        self.assertEqual(self.graph, carver.build_graph(image))
-
-    def test_get_shortest_path(self):
-        image = self._create_test_image()
-
-        self.assertEqual(carver.get_shortest_path(self.graph), self._get_shortest_seam_index())
+    def test_get_shortest_vertical_path(self):
+        self.assertEqual(carver.get_shortest_vertical_path(graph), get_shortest_seam_index())
 
     def test_remove_first_column(self):
-        matrix = deepcopy(self.test_matrix)
-        copy = deepcopy(self.graph[:])
-        original = deepcopy(self.graph[:])
+        matrix = deepcopy(test_matrix)
+        copy = deepcopy(graph[:])
+        original = deepcopy(graph[:])
 
-        copy[0].pop(0)
-        copy[1].pop(0)
-        copy[2].pop(0)
-        copy[3].pop(0)
+        for i in range(4):
+            copy[i].pop(0)
 
         self.assertEqual(copy, carver._remove_column_from_graph(matrix, original, 0))
 
     def test_remove_last_column(self):
-        matrix = deepcopy(self.test_matrix)
+        matrix = deepcopy(test_matrix)
 
-        copy = deepcopy(self.graph[:])
-        original = deepcopy(self.graph[:])
+        copy = deepcopy(graph[:])
+        original = deepcopy(graph[:])
 
-        copy[0].pop(2)
-        copy[1].pop(2)
-        copy[2].pop(2)
-        copy[3].pop(2)
-
+        for i in range(4):
+            copy[i].pop(2)
 
         self.assertEqual(copy, carver._remove_column_from_graph(matrix, original, 2))
 
     def test_remove_one_seam(self):
-        image = self._create_test_image()
-        graph_copy = deepcopy(self.graph)
-        matrix_copy = deepcopy(self.test_matrix)
+        image = create_test_image()
+        graph_copy = deepcopy(graph)
+        matrix_copy = deepcopy(test_matrix)
 
         self.assertEqual(carver._remove_column_from_graph(matrix_copy, graph_copy, 0),
                          carver.remove_shortest_seams(image, 1))
-
-
